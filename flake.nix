c2607d8202059d0855698181cfc8b08f16143cba
Jake Woods <jake@jakewoods.net>
2024-02-09 17:48:20 +1100

    Add basic flake
    Lets get our build env going

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            (pkgs.callPackage ./nix/sfml-1.6 {})
+            pkgs.libGL
+            pkgs.libGL.dev
+            pkgs.libglvnd
+            pkgs.libglvnd.dev
+            pkgs.mesa
+            pkgs.mesa_glu
+          ];
+        };
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+      };
+    };
+}
