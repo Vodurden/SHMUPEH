5746b0030ef90929eaefe3a9780d1d7e8a9d7add
Jake Woods <jake@jakewoods.net>
2024-02-10 15:03:24 +1100

    Game now launches
    And segfaults

--- src/EnemyWaveSpawner.h
+++ src/EnemyWaveSpawner.h
@@ -9,15 +9,27 @@ class EnemyWaveSpawnerBase
     protected:
         Enemy* base;
         EnemyFactory* enemyFactory;
-        int amount; // Max amount of enemies spawned by this spawner
-        int currentAmount; // Current amount of enemies spawned by this spawner
         float spawnInterval; // Interval in seconds between enemy spawns
+        int spawnAmount; // Max amount of enemies spawned by this spawner
+        int currentAmount; // Current amount of enemies spawned by this spawner
         bool started; // Yes if the wave has started spawning
         sf::Clock spawnTimer;
     public:
-        void think();
-        bool isDone();
+        EnemyWaveSpawnerBase(Enemy* base, EnemyFactory* enemyFactory, float spawnInterval, int spawnAmount);
+        virtual void think() = 0;
+        bool isDone() { return currentAmount >= spawnAmount; };
 };
 
+template <class T> class EnemyWaveSpawner : public EnemyWaveSpawnerBase
+{
+    public:
+        EnemyWaveSpawner(Enemy* base, EnemyFactory* enemyFactory, float spawnInterval, int spawnAmount) : EnemyWaveSpawnerBase(base, enemyFactory, spawnInterval, spawnAmount) {}
+        void think();
+};
+
+template <class T> void EnemyWaveSpawner<T>::think()
+{
+    // TODO
+}
 
 #endif // ENEMYWAVESPAWNER_H_
--- src/EnemyWaveSpawnerFactory.h
+++ src/EnemyWaveSpawnerFactory.h
@@ -13,10 +13,15 @@ class EnemyWaveSpawnerFactory
     public:
         EnemyWaveSpawnerFactory(EnemyFactory* eF) {eFact = eF;}
         ~EnemyWaveSpawnerFactory();
-        template <class T> void addWave(Enemy* base, float Interval, int Amount);
+        template <class T> void addWave(Enemy* base, float interval, int amount);
         void addBaseWave(EnemyWaveSpawnerBase* waveSpawner);
         void reset();
         void think();
 };
+
+template <class T> void EnemyWaveSpawnerFactory::addWave(Enemy* base, float interval, int amount)
+{
+    tWaves.push_back(new EnemyWaveSpawner<T>(base, eFact, interval, amount));
+}
 
 #endif // ENEMYWAVESPAWNERFACTORY_H_
